932c957696614e42f5af38abd758eff4997320d0
VeneraAbd <ven.abdyganyeva@gmail.com>
2024-03-21 16:16:44 +0600

    profile page layout

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from "./Card.module.css";
+import like_icon from "../../assets/like_white.svg";
+import save_icon from "../../assets/save_white.svg";
+import dish_img from "../../assets/details-hero.jpeg";
 
-const Card = ({data}) => {
-    return (
-        <>
-            {data && data.map((item, index) => (
-                <Link key={index} to={`/detailpage/${item.id}`} className={styles.cardLink}>
-                    <div className={styles.card}>
-                        <img src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-1640772.jpg&fm=jpg" alt="recipies" />
-                        {/* <img src={item.image} alt={item.city} /> */}
-                        <h3 className={styles.cardTitle}>{`${item.nameOfThePlace}`}</h3>
-                    </div>
-                </Link>
-            ))}
-        </>
-    );
+const Card = ({ title, author, likes, saves }) => {
+  return (
+    <div className={styles.card} style={{ backgroundImage: `url(${dish_img})` }}>
+      <div className={styles.overlay}>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Egg Omlet {title}</h2>
+          <p className={styles.author}>by Ainsley Harriott {author}</p>
+          <div className={styles.icons}>
+            <span className={styles.icon}><img src={like_icon} alt="like icon" className={styles.icon_img}/>118{likes}</span>
+            <span className={styles.icon}><img src={save_icon} alt="save icon" className={styles.icon_img}/>118{saves}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
+import styles from "./ProfilePage.module.css";
+import Card from "../../components/Card/Card";
 // import '../styles/profile.css'
 
 const ProfilePage = () => {
-  const [firstName] = useState('John')
 
   return (
-    <div className='unauthorized'>
-      <figure>{firstName.charAt(0).toUpperCase()}</figure>
-      <span>
-        Welcome <strong>{firstName}!</strong> You can view this page because
-        you're logged in
-      </span>
+    <div className={styles.profile_container}>
+        <h2 className={styles.profile}>Profile</h2>
+        
+        <div className={styles.wrapper}>
+          <img src="https://newprofilepic.photo-cdn.net//assets/images/article/profile.jpg?90af0c8" alt="Profile photo" className={styles.profile_photo}/>
+          <div className={styles.profile_info}>
+            <div className={styles.followers}>
+              <div className={styles.number}>29 <p className={styles.follow}>Recipe</p></div>
+              <div className={styles.number}>144 <p className={styles.follow}>Followers</p></div>
+              <div className={styles.number}>100 <p className={styles.follow}>Following</p></div>
+            </div>
+            <h4 className={styles.full_name}>Sarthak Ranjan Hota</h4>
+            <p className={styles.description}>I'm a passionate chef who loves creating delicious dishes with flair.</p>
+            <button className={styles.edit_profile_button}>Manage Profile</button>
+          </div>
+        </div>
+
+        <div className={styles.recipe_types}>
+          <p>My recipe</p>
+          <p>Saved recipe</p>
+        </div>
+        
+        <div className={styles.posts}>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
     </div>
   )
 }
